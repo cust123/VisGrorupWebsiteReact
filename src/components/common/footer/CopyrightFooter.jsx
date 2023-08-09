56d88f46f02b792ac7048f603cac6fc60d78c27a
Ubaid Ullah <khanxx777@gmail.com>
2023-08-09 13:04:40 +0500

    asdas

--- src/components/common/footer/CopyrightFooter.jsx
+++ src/components/common/footer/CopyrightFooter.jsx
@@ -4,7 +4,7 @@ import CopyrightSocial from "./CopyrightSocial";
 
 const CopyrightFooter = () => {
   const footerList = [
-    { id: 1, name: "Privacy & Terms.", routeLink: "/privacy" },
+    { id: 1, name: "Privacy Policy", routeLink: "/privacy" },
     { id: 2, name: "Contact Us", routeLink: "/contact-us" },
   ];
   return (
@@ -12,7 +12,7 @@ const CopyrightFooter = () => {
       <div className="col-lg-4 order-lg-0 mb-15">
         <ul className="d-flex justify-content-center justify-content-lg-start footer-nav style-none">
           {footerList.map((list) => (
-            <li key={list.id}>
+            <li key={list.id} style={{ fontFamily: "Recoleta" }}>
               <Link to={list.routeLink}>{list.name}</Link>
             </li>
           ))}
@@ -28,17 +28,16 @@ const CopyrightFooter = () => {
       {/* End .col */}
 
       <div className="col-lg-4 order-lg-1 mb-15">
-        <p className="copyright text-center">
+        <p className="copyright text-center" style={{ fontFamily: "Recoleta" }}>
           {" "}
-          Copyright @{new Date().getFullYear()}{" "}
+          Copyright @ {new Date().getFullYear()}{" "}
           <a
             href="https://themeforest.net/user/ib-themes/portfolio"
             target="_blank"
             rel="noopener noreferrer"
           >
-            ib-themes
+            Vis-Group
           </a>{" "}
-          Ltd.
         </p>
       </div>
       {/* End .col */}
